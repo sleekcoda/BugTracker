e6862393c7030966ccbb9728b553482196e8889b
Toheeb Ogunleye <dapoogunleye007@gmail.com>
2022-08-17 15:51:25 +0100

    fix: tickets controllers queries

--- controllers/userProjectController.js
+++ controllers/userProjectController.js
@@ -1,31 +1,46 @@
-const pool = require("../db");
+const {
+  userProjects,
+  projects,
+  users
+} = require("../prisma/client");
 
 module.exports = {
   assignUser: async (req, res) => {
-    const { projectId } = req.params;
-    const { userId } = req.body; // If there are multiple users to assign, this will be handled on front end
-    const client = await pool.connect();
+    const {
+      projectId
+    } = req.params;
+    const {
+      userId
+    } = req.body; // If there are multiple users to assign, this will be handled on front end
+    // const client = await pool.connect();
 
     try {
-      await client.query(
-        "INSERT INTO user_projects (project_id, user_id) VALUES ($1, $2)",
-        [projectId, userId]
-      );
 
-      res
+      const row = await userProjects.create({
+        data: {
+          userId,
+          projectId
+        }
+      })
+      console.log("Added user", userId, "to project", projectId)
+      return res
         .status(201)
-        .json({ msg: `User ${userId} assigned to ${projectId} succesfully` });
+        .json({
+          msg: `Assigned user to project succesfully`
+        });
     } catch (err) {
       console.log("assignUsers query error: ", err);
-      res
+      return res
         .status(400)
-        .json({ msg: "Please review user project assign creation query" });
-    } finally {
-      await client.release();
+        .json({
+          msg: "Please review user project assign creation query"
+        });
     }
   },
   removeAllUsers: async (req, res) => {
-    const { projectId } = req.params;
+    const {
+      projectId
+    } = req.params;
     const client = await pool.connect();
 
     try {
@@ -38,49 +53,65 @@ module.exports = {
       console.log("getProject query error: ", err);
       res
         .status(500)
-        .json({ msg: "Unable to remove user_project from database" });
-    } finally {
-      await client.release();
+        .json({
+          msg: "Unable to remove user_project from database"
+        });
     }
   },
   removeUser: async (req, res) => {
-    const { projectId, userId } = req.params;
-    const client = await pool.connect();
-
+    const {
+      projectId,
+      userId
+    } = req.params;
+    console.log(projectId, userId)
     try {
-      await client.query(
-        "DELETE FROM user_projects WHERE project_id = $1 AND user_id = $2",
-        [projectId, userId]
-      );
+      const {
+        id
+      } = await userProjects.findFirst({
+        where: {
+          projectId,
+          userId
+        }
+      })
+      console.log(id)
+      const row = await userProjects.delete({
+        where: {
+          id
+        }
+      })
 
       res.status(202).json(`User removed from project`);
     } catch (err) {
       console.log("getProject query error: ", err);
-      res
-        .status(500)
-        .json({ msg: "Unable to remove user_project from database" });
-    } finally {
-      await client.release();
+      res.status(500)
+        .json({
+          msg: "Unable to remove user_project from database"
+        });
     }
   },
   getProjectUsers: async (req, res) => {
-    const { projectId } = req.params;
-    const client = await pool.connect();
+    const {
+      projectId
+    } = req.params;
 
     try {
-      const {
-        rows,
-      } = await client.query(
-        "SELECT user_id, first_name, last_name, phone, email FROM users JOIN user_projects ON (user_projects.user_id = users.id) WHERE project_id = $1",
-        [projectId]
-      );
 
-      res.json(rows);
+      const rows = await userProjects.findMany({
+        where: {
+          projectId
+        },
+        select:{
+          User:true 
+        }
+      })
+      const users = rows.map((row) => row.User)
+
+      res.json(users);
     } catch (err) {
       console.log("getProjectUsers query error: ", err);
-      res.status(400).json({ msg: "Please review query" });
-    } finally {
-      client.release();
-    }
+      res.status(400).json({
+        msg: "Please review query"
+      });
+    } 
   },
 };
